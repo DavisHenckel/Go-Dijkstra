70d9a30843aeef9e7c1a0bf038e8bc183629f158
DavisHenckel <davishenckel@hotmail.com>
2022-09-23 13:02:32 -0700

    build contains function, fix small bug

--- Algorithm/Dijkstra.go
+++ Algorithm/Dijkstra.go
@@ -30,23 +30,36 @@ func FindShortestPath(graph [][]int, startNode int, endNode int) []int {
 		//loop through the current nodes connections
 		for k := range currentNodeConnections {
 			//if this nodes connection is shorter than the current map of distances, update
-			if currentNodeConnections[k] > distancesToNodes[k] { //CHECK THIS
+			if currentNodeConnections[k] < distancesToNodes[k] { //CHECK THIS
 				distancesToNodes[k] = currentNodeConnections[k]
 			}
 		}
 		pathTaken = append(pathTaken, currentNode)
-		currentNode = GetShortestPathFromNode(currentNodeConnections)
+		currentNode = GetShortestPathFromNode(currentNodeConnections, pathTaken)
+		if currentNode == math.MaxInt {
+			currentNode = pathTaken[ctr-1]
+		}
+		ctr++
 	}
 	return pathTaken
 }
 
 // returns index of shortest path from a given node
-func GetShortestPathFromNode(slice []int) int {
+func GetShortestPathFromNode(slice []int, nodesVisited []int) int {
 	shortest := math.MaxInt
 	for i := range slice {
-		if (slice[i] < shortest) && slice[i] != 0 {
+		if ((slice[i] < shortest) && slice[i] != 0) && Contains(nodesVisited, i) == false {
 			shortest = i
 		}
 	}
 	return shortest
+}
+
+func Contains(slice []int, element int) bool {
+	for i := range slice {
+		if slice[i] == element {
+			return true
+		}
+	}
+	return false
 }
