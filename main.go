7d1675d9e22a163b3b91ad6ff9e301a8e9fef7b2
DavisHenckel <davishenckel@hotmail.com>
2022-09-24 02:22:41 -0700

    Account for case where travel is not possible

--- main.go
+++ main.go
@@ -7,28 +7,35 @@ import (
 )
 
 func main() {
+	startNode := 1
+	endNode := 10
 	fmt.Println("Welcome to Davis Henckel's implementation of Dijkstra's Algorithm written in Go.\n================================================================================")
 	numNodes := AdjacencyMatrix.PickNumNodes()
 	fmt.Println("\nThe number of nodes in this graph is", numNodes)
 	Graph := AdjacencyMatrix.GenerateMatrix(numNodes)
 	AdjacencyMatrix.PrintMatrix(Graph, numNodes)
-	finalNode := Algorithm.FindShortestPath(Graph, 1, 10)
-	fmt.Println("Found the shortest path to node ", finalNode.Name, ", the distance was ", finalNode.Distance)
-	pathTaken := make([]int, 0)
-	pathTaken = append(pathTaken, finalNode.Name)
-	tmp := finalNode.Previous
-	for tmp != nil {
-		pathTaken = append(pathTaken, tmp.Name)
-		tmp = tmp.Previous
-	}
-	fmt.Print("The path was: ")
-	i := len(pathTaken) - 1
-	for i >= 0 {
-		if i == 0 {
-			fmt.Println(pathTaken[i])
-		} else {
-			fmt.Print(pathTaken[i], "->")
+	finalNode := Algorithm.FindShortestPath(Graph, startNode, endNode)
+	if finalNode != nil {
+		fmt.Println("Found the shortest path to node ", finalNode.Name, ", the distance was ", finalNode.Distance)
+		pathTaken := make([]int, 0)
+		pathTaken = append(pathTaken, finalNode.Name)
+		tmp := finalNode.Previous
+		for tmp != nil {
+			pathTaken = append(pathTaken, tmp.Name)
+			tmp = tmp.Previous
 		}
-		i--
+		fmt.Print("The path was: ")
+		i := len(pathTaken) - 1
+		for i >= 0 {
+			if i == 0 {
+				fmt.Println(pathTaken[i])
+			} else {
+				fmt.Print(pathTaken[i], "->")
+			}
+			i--
+		}
+	} else {
+		fmt.Println("It was not possible to travel from nodes ", startNode, "->", endNode)
 	}
+
 }
