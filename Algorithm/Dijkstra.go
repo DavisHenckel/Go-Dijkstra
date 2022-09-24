4aa59af2f6be95e572fa31fddae8d08789df32ac
DavisHenckel <davishenckel@hotmail.com>
2022-09-24 02:08:58 -0700

    export struct and its members properly

--- Algorithm/Dijkstra.go
+++ Algorithm/Dijkstra.go
@@ -11,52 +11,52 @@ STARTNODE = The node to start travelling from
 ENDNODE = The node to travel to
 */
 //Returns an array that displays the shortest path taken.
-func FindShortestPath(graph [][]int, startNode int, endNode int) *graphNode {
+func FindShortestPath(graph [][]int, startNode int, endNode int) *GraphNode {
 	//initialize the priority queue using a min heap.
 	prioQueue := make(PriorityQueue, 0)
 	heap.Init(&prioQueue)
-	var finalNode *graphNode
+	var finalNode *GraphNode
 	//hashmap to keep track of overall distances.
 	var distancesToNodes = map[int]int{}
 	currentNodeConnections := graph[startNode]
-	//add all nodes to priority queue with int max as the distance
+	//add all nodes to priority queue with int max as the Distance
 	for i := range graph {
 		if i == 0 {
-			ithNode := graphNode{name: startNode, previous: nil, distance: 0, index: i}
+			ithNode := GraphNode{Name: startNode, Previous: nil, Distance: 0, Index: i}
 			heap.Push(&prioQueue, &ithNode)
 			distancesToNodes[startNode] = 0
 		} else {
 			distancesToNodes[i] = math.MaxInt
-			ithNode := graphNode{name: i, previous: nil, distance: math.MaxInt, index: i}
+			ithNode := GraphNode{Name: i, Previous: nil, Distance: math.MaxInt, Index: i}
 			heap.Push(&prioQueue, &ithNode)
 			distancesToNodes[i] = math.MaxInt
 		}
 	}
 	//loop through every node in the graph
 	for len(prioQueue) > 0 {
-		currentNode := heap.Pop(&prioQueue).(*graphNode)
-		currentNodeConnections = graph[currentNode.name]
+		currentNode := heap.Pop(&prioQueue).(*GraphNode)
+		currentNodeConnections = graph[currentNode.Name]
 		//loop through the current nodes connections
 		for i := range currentNodeConnections {
 			if currentNodeConnections[i] != 0 {
 				// get the node in question
-				var nodeToUpdate *graphNode
+				var nodeToUpdate *GraphNode
 				j := 0
 				for j < len(prioQueue) {
 					tmp := prioQueue[j]
-					if tmp.name == i {
+					if tmp.Name == i {
 						nodeToUpdate = tmp
 						break
 					}
 					j++
 				}
-				//update the node only if the distance has changed
+				//update the node only if the Distance has changed
 				//if this nodes connection is shorter than the current map of distances, update the hashmap
-				if currentNodeConnections[i]+currentNode.distance < distancesToNodes[i] {
-					distancesToNodes[i] = currentNodeConnections[i] + currentNode.distance
+				if currentNodeConnections[i]+currentNode.Distance < distancesToNodes[i] {
+					distancesToNodes[i] = currentNodeConnections[i] + currentNode.Distance
 					if nodeToUpdate != nil {
-						prioQueue.update(nodeToUpdate, currentNodeConnections[i]+currentNode.distance, currentNode)
-						if endNode == nodeToUpdate.name {
+						prioQueue.update(nodeToUpdate, currentNodeConnections[i]+currentNode.Distance, currentNode)
+						if endNode == nodeToUpdate.Name {
 							finalNode = nodeToUpdate
 						}
 					}
@@ -79,7 +79,7 @@ func removeElement(slice []int, element int) []int {
 	return append(returnSlice)
 }
 
-// returns index of shortest path from a given node
+// returns Index of shortest path from a given node
 func GetShortestPathFromNode(slice []int, nodesVisited []int) int {
 	shortest := math.MaxInt
 	for i := range slice {
